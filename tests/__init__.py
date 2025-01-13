1e7bb0d260b306cb7af7c3631769857ed1d5e2e3
Joerassic <johanne.cornelius@mac.com>
2025-01-13 11:50:24 +0100

    this will work

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
 import os
 _TEST_ROOT = os.path.dirname("joe_project/tests")  # root of test folder
 _PROJECT_ROOT = os.path.dirname("joe_project/src")  # root of project
-_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
+_PATH_DATA = os.path.join("joe_project/data")  # root of data
